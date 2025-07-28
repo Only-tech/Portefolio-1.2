11ef18e3da06c740339522ca53db482a77133254
Cédrick F <kcedrick185@gmail.com>
2025-07-28 14:44:23 +0200

    updating scroll to top js

--- assets/js/onTopButton.js
+++ assets/js/onTopButton.js
@@ -1,13 +1,30 @@
 window.addEventListener("scroll", function() {
-    let scrollTopBtn = document.getElementById("scrollTopBtn");
+    let scrollProgressWrapper = document.getElementById("scrollProgressWrapper");
 
     if (window.scrollY > 500) {
-        scrollTopBtn.style.display = "flex";
+        scrollProgressWrapper.style.display = "flex";
     } else {
-        scrollTopBtn.style.display = "none";
+        scrollProgressWrapper.style.display = "none";
     }
 });
 
-document.getElementById("scrollTopBtn").addEventListener("click", function() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-});
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+document.addEventListener('scroll', updateScrollProgress);
+
+function updateScrollProgress() {
+    const circle = document.getElementById('progressActive');
+
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollRatio = scrollHeight === 0 ? 0 : scrollTop / scrollHeight;
+
+    const radius = circle.r.baseVal.value;
+    const circumference = 2 * Math.PI * radius;
+    const offset = circumference * (1 - scrollRatio);
+
+    circle.style.strokeDasharray = `${circumference}`;
+    circle.style.strokeDashoffset = `${offset}`;
+}
